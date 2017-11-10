fc75d616498c328df37a350eace153a07b684944
Christopher Mullins <christopherrmullins@gmail.com>
2017-11-10 16:10:07 -0500

    Update for react

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,7 @@ import os
 import datetime
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # instantiate the db
@@ -11,6 +12,9 @@ def create_app():
 
 	# instantiate the app
 	app = Flask(__name__)
+
+	# enable CORS
+	CORS(app)
 
 	# set config
 	app_settings = os.getenv('APP_SETTINGS')
